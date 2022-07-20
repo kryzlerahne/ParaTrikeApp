54daf3a063137878e4423259af6a608f408e779d
Jomark Cornejo <jomarkcornejo12@gmail.com>
2022-07-21 00:36:05 +0800

    created new map page

---
+++ src/app/map/map.page.ts
@@ -0,0 +1,60 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap, Marker } from '@capacitor/google-maps';
+import { ModalController } from '@ionic/angular';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.page.html',
+  styleUrls: ['./map.page.scss'],
+})
+export class MapPage {
+  @ViewChild('map')mapRef: ElementRef;
+  map: GoogleMap;
+
+  constructor() { }
+
+  ionViewDidEnter() {
+    this.createMap();
+  }
+
+  async createMap() {
+    this.map = await GoogleMap.create({
+      id: 'my-map',
+      apiKey: environment.mapsKey,
+      element: this.mapRef.nativeElement,
+      config: {
+        center: {
+          lat: 13.7758,
+          lng: 121.0455,
+        },
+        zoom: 19,
+      },
+    });
+    await this.addMarkers();
+  }
+
+  async addMarkers() {
+    const markers: Marker[] = [ //add lang dine ng element sa array para magka marker or delete ung element para tagtagin marker
+      {
+        coordinate: {
+          lat: 13.7756,
+          lng: 121.0455,
+        },
+        title: 'localhost', //para sa modal pero di naman gagamit nun hahaha
+        snippet: 'Best place on earth',
+      },
+      {
+        coordinate: {
+          lat: 13.7754,
+          lng: 121.0455,
+        },
+        title: 'title',
+        snippet: 'Not sure',
+      },
+    ];
+
+    const result = await this.map.addMarkers(markers);
+    console.log(result);
+  }
+}
