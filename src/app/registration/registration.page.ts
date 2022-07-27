0d6c67e1d48ffa0df0f512037b52d8d52214e075
Kryzle Rahne Enriquez <kryzlerahne.enriquez@g.batstate-u.edu.ph>
2022-07-27 14:21:08 +0800

    Added Form and Input Validation for Registration Page 1

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,17 +10,61 @@ import { Router } from '@angular/router';
 })
 export class RegistrationPage implements OnInit {
 
-  constructor(private router: Router) { }
-
-  ngOnInit() {
+  get firstName() {
+    return this.registrationForm.get('firstName');
   }
 
-  register() {
-  this.router.navigate(['/home']);
-}
+  get lastName() {
+    return this.registrationForm.get('lastName');
+  }
 
-  next(){
+  get email() {
+    return this.registrationForm.get('email');
+  }
+
+  get mobileNum() {
+    return this.registrationForm.get('mobileNum');
+  }
+
+  public errorMessages = {
+    firstName: [
+      { type: 'required', message: 'First Name is required' }
+    ],
+    lastName: [
+      { type: 'required', message: 'Last Name is required' }
+    ],
+    email: [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Please enter a valid email address' }
+    ],
+    mobileNum: [
+      { type: 'required', message: 'Mobile Number is required' },
+      { type: 'pattern', message: 'Please enter a valid mobile number' },
+    ]
+  };
+
+  registrationForm = this.formBuilder.group({
+    firstName : ['', [Validators.required]],
+    lastName : ['', [Validators.required]],
+    email : ['', [Validators.required,
+    Validators.pattern('^[a-zA-Z0-9._%-]+@[a-zA-Z0-9•-]+.[a-zA-Z]{2,4}$')
+  ]],
+    mobileNum : ['', [Validators.required,
+    Validators.pattern('^[+]*[(]{0,1}[0,9]{1,4}[)]{0,1}[-s./0-9]*$'),
+    Validators.minLength(11)
+    ]]
+  });
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
+
+  public submit() {
+    if(!this.registrationForm.valid){
+      this.registrationForm.markAllAsTouched();
+      return;
+    }
     this.router.navigate(['/registration-account']);
   }
 
+  ngOnInit() {
+  }
 }
