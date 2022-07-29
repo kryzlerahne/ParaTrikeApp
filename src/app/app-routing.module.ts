abe9e85ead7020209beadc1473a6c739986f6e8a
Kryzle Rahne Enriquez <kryzlerahne.enriquez@g.batstate-u.edu.ph>
2022-07-30 02:35:42 +0800

    Added Payment Page Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -51,6 +51,10 @@ const routes: Routes = [
     path: 'history',
     loadChildren: () => import('./history/history.module').then( m => m.HistoryPageModule)
   },
+  {
+    path: 'payment',
+    loadChildren: () => import('./payment/payment.module').then( m => m.PaymentPageModule)
+  },
 ];
 
 @NgModule({
