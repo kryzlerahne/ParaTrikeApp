a15cdaa73b00e1e13ede1d0b4358f87a5f2eb4e2
Kryzle Rahne Enriquez <kryzlerahne.enriquez@g.batstate-u.edu.ph>
2022-07-30 02:36:57 +0800

    Created Payment Page UI

---
+++ src/app/payment/payment.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.page.html',
+  styleUrls: ['./payment.page.scss'],
+})
+export class PaymentPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  back() {
+      this.router.navigate(['/trip-details']);
+  }
+
+  pay(){
+    this.router.navigate(['/confirmation']);
+  }
+
+  ngOnInit() {
+  }
+
+}
