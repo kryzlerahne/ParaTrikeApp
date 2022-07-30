adbebc1d8f8607bf940d3df897777dc5179773f2
Kryzle Rahne Enriquez <kryzlerahne.enriquez@g.batstate-u.edu.ph>
2022-07-31 00:00:45 +0800

    Updated Confirmation Page and Added Modal Popup for Cancellation

--- src/app/confirmation/confirmation.page.ts
+++ src/app/confirmation/confirmation.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.page.html',
   styleUrls: ['./confirmation.page.scss'],
 })
-export class ConfirmationPage implements OnInit {
+export class ConfirmationPage {
 
-  constructor() { }
+  @ViewChild(IonModal) modal: IonModal;
 
-  ngOnInit() {
+  constructor(private router: Router) { }
+
+  complete() {
+      this.router.navigate(['/map']);
+  }
+
+  cancel() {
+      this.router.navigate(['/cancellation']);
+      this.modal.dismiss();
   }
 
 }
